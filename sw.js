73549bfdebe9072477f0d75adbaf44bda7376c6a
Imran Nur Hirey <95400512+imranhirey@users.noreply.github.com>
2022-06-20 04:26:35 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,21 @@
-console.log('service worker')
+let cachname='v1'
+
+self.addEventListener('install',(e)=>{
+   
+    
+})
+
+self.addEventListener('fetch',e=>{
+   e.respondWith(
+       fetch(e.request)
+       .then(res=>{
+           const resclone= res.clone()
+           caches.open(cachname).then(cache=>{
+               cache.put(e.request,resclone)
+           })
+           return res
+       })
+       .catch(err=>caches.match(e.request).then(res=>res))
+
+   )
+})
